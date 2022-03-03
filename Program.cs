a453789b0d87f32f09d23f87e3e3f48be17c8338
Fatima1899 <99797429+Fatima1899@users.noreply.github.com>
2022-03-03 08:17:39 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MonthProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String month = "December";
+
+            if (month == "December" || month == "January" || month == "February")
+                Console.WriteLine("Winter");
+            else if (month == "March" || month == "April" || month == "May")
+                Console.WriteLine("Spring");
+            else if (month == "June" || month == "July" || month == "August")
+                Console.WriteLine("Summer");
+            else if (month == "September" || month == "October" || month == "December")
+                Console.WriteLine("Autumn");
+        }
+    }
+}
